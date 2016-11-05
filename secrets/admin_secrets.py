764b8917fe9502907f0c59465ec690a4953af8ef
Elijah Caluya <elijahcaluya@128-223-223-70-wireless.uoregon.edu>
2016-11-04 17:02:43 -0700

    removed secrets

--- secrets/admin_secrets.py
+++
@@ -1,6 +0,0 @@
-# Admin credentials
-
-admin_user = "elijahcaluya"
-admin_pw = "password123"
-port = 27017
-host = "admin"
--- secrets/client_secrets.py
+++
@@ -1,5 +0,0 @@
-# Client credentials
-
-db = "memos"
-db_user = "user1"
-db_user_pw = "password1"
